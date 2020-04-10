9c3fe2a2bf25bc5a59a5ad2bb041ac08e9deb396
Michael Tanner <michaelmontytanner@gmail.com>
2020-04-09 20:11:18 -0500

    Updates

---
+++ precache-manifest.add9641d3d4461afa7b69dadc00cec3b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7aaee6eba6ed3cf9fed23f970ac138",
+    "url": "/dollardonuts/index.html"
+  },
+  {
+    "revision": "20bda86a712ebfd0ba13",
+    "url": "/dollardonuts/static/css/main.543804a8.chunk.css"
+  },
+  {
+    "revision": "e58edc36a77a1820c610",
+    "url": "/dollardonuts/static/js/2.1ab71789.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dollardonuts/static/js/2.1ab71789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bda86a712ebfd0ba13",
+    "url": "/dollardonuts/static/js/main.9b5250b7.chunk.js"
+  },
+  {
+    "revision": "0694ca2b8429dadbeb56",
+    "url": "/dollardonuts/static/js/runtime-main.ceab0da1.js"
+  },
+  {
+    "revision": "2a09b6868d72eb1c0b4dc54912a2b728",
+    "url": "/dollardonuts/static/media/Logo.2a09b686.svg"
+  },
+  {
+    "revision": "0db8e097e8517ee862d871cda2995d1b",
+    "url": "/dollardonuts/static/media/baker.0db8e097.jpg"
+  },
+  {
+    "revision": "e9782f4959064a6ebb6216029c0c00e9",
+    "url": "/dollardonuts/static/media/cashier.e9782f49.jpg"
+  },
+  {
+    "revision": "90c435cb0de5251c2ac2761603a664bd",
+    "url": "/dollardonuts/static/media/coffee.90c435cb.jpg"
+  },
+  {
+    "revision": "8f87c1aae0eb413bcb19bed949645358",
+    "url": "/dollardonuts/static/media/donut_placeholder.8f87c1aa.png"
+  },
+  {
+    "revision": "247186d417877b593b818f6c171ce718",
+    "url": "/dollardonuts/static/media/donuts.247186d4.jpg"
+  },
+  {
+    "revision": "eff61d47a0b4c4034991760a756afb49",
+    "url": "/dollardonuts/static/media/hero_bg.eff61d47.jpg"
+  },
+  {
+    "revision": "1ac7791ff922b1be6dbb92134efd400b",
+    "url": "/dollardonuts/static/media/hero_bg_wide.1ac7791f.jpg"
+  },
+  {
+    "revision": "b0c75df3581e233442e1f84e0f938ac3",
+    "url": "/dollardonuts/static/media/hiring_desktop.b0c75df3.jpg"
+  },
+  {
+    "revision": "c6b9c7d98d2c57e2870a9b553a16b8da",
+    "url": "/dollardonuts/static/media/kolaches.c6b9c7d9.png"
+  },
+  {
+    "revision": "a206260713080da0a6b8ebb1590ce701",
+    "url": "/dollardonuts/static/media/shop_info_bg.a2062607.jpg"
+  }
+]);
